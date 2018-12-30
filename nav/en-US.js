f3e7b8571d3204e8b733719a9b5e9bb9e3e89d0b
Dean <dean@airdb.com>
2018-12-30 11:28:51 +0800

    update

---
+++ nav/en-US.js
@@ -0,0 +1,15 @@
+module.exports = [
+  { text: 'bbhj', link: '/zh-CN/bbhj/' },
+  {
+    text: 'more',
+    items: [
+      {
+	text: 'Open Plubic Tech Class',
+        items: [
+          { text: 'Linux - how to', link: '/zh-CN/open/linux/' },
+          { text: 'Golang - low level', link: '/zh-CN/open/golang/' },
+        ]
+      }
+    ]
+  }
+]
