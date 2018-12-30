1e2c1daac2ef88febc691807893f9fdfce557b81
Dean <dean@airdb.com>
2018-12-30 13:57:19 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -7,5 +7,5 @@ echo 'www.airdb.com' > CNAME
 
 git init &&
 git add -A  &&
-git commit -m"update deploy" &&
-git push -f https://github.com/airdb/airdb.github.io.git master
+git commit -m"update deploy"
+#git push -f https://github.com/airdb/airdb.github.io.git master
